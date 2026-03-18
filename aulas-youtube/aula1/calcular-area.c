211658cb16ed69e0947f35467b097b299abfe864
abilioteste <abiliodevtestes@gmail.com>
2026-03-18 03:20:23 +0000

    criando constante em pi

--- aulas-youtube/aula1/calcular-area.c
+++ aulas-youtube/aula1/calcular-area.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 void main(){
-    float area, raio, pi;
-    pi = 3.14;
+    const float Pi = 3.14;
+    float area, raio;
     printf("Digite o raio do circulo: ");
     scanf("%f", &raio);
-    area =  pi * raio * raio;
+    area =  Pi * raio * raio;
     printf("O valor da area eh: %.2f\n", area);
 }
