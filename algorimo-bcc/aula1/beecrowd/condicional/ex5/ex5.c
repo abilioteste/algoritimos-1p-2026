89d077f83e1c8432cf22821e716d641205e14bd9
abilioteste <abiliodevtestes@gmail.com>
2026-04-06 15:20:17 +0000

    otimizado algoritimo da media

--- algorimo-bcc/aula1/beecrowd/condicional/ex5/ex5.c
+++ algorimo-bcc/aula1/beecrowd/condicional/ex5/ex5.c
@@ -4,11 +4,10 @@ int main() {
     float n1, n2, n3, n4, n5, media;
     scanf("%f %f %f %f", &n1, &n2, &n3, &n4);
     media = ((n1*2) + (n2*3) + (n3*4) + (n4*1)) / 10;
+    printf("Media: %.1f\n", media);
     if (media >= 7){
-        printf("Media: %.1f\n", media);
         printf("Aluno aprovado.\n");
     } else if (media >= 5){
-        printf("Media: %.1f\n", media);
         scanf("%f", &n5);
         printf("Aluno em exame.\n");
         printf("Nota do exame: %.1f\n", n5);
@@ -22,7 +21,6 @@ int main() {
         }
     }
     else {
-        printf("Media: %.1f\n", media);
         printf("Aluno reprovado.\n");
     }
     
